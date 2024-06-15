397070e0ac7d6d2a2862844706b6b431d4a38274
Kali Lohse <kali.lechner@yahoo.com>
2024-06-14 20:26:57 -0400

    Creating login page

--- app/src/main/java/com/example/myapplication/UI/MainActivity.java
+++ app/src/main/java/com/example/myapplication/UI/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,26 +17,37 @@ import com.example.myapplication.R;
 
 public class MainActivity extends AppCompatActivity {
     public static int numAlert;
-
+    private EditText editUsername, editPassword;
+    private Button submitbutton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        Button button=findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+
+        editUsername = findViewById(R.id.editUsername);
+        editPassword = findViewById(R.id.editPassword);
+        submitbutton = findViewById(R.id.submitbutton);
+        submitbutton.setOnClickListener(new View.OnClickListener() {
 
                                       @Override
                                       public void onClick(View v) {
-                                          Intent intent = new Intent(MainActivity.this, VacationList.class);
-                                          intent.putExtra("test", "Information sent");
-                                          startActivity(intent);
-                                      }
-                                  });
-         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-                Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-                return insets;
-            });
+
+                                              String username = editUsername.getText().toString().trim();
+                                              String password = editPassword.getText().toString().trim();
+
+                                              if (username.equals("username") && password.equals("password")) {
+                                                  Intent intent = new Intent(MainActivity.this, VacationList.class);
+                                                  startActivity(intent);
+                                                  finish();
+                                              } else {
+                                                  Toast.makeText(MainActivity.this, "Incorrect username or password",
+                                                          Toast.LENGTH_LONG).show();
+                                              }
+                                          }
+                                      });
         }
     }
+
+
+
